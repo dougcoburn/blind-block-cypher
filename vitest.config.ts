9b4d557dc4c79f85e443f6bbbe751311169a55d7
Doug Coburn <dougcobie@gmail.com>
2023-06-19 17:39:03 -0600

    require perfect coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,14 @@ export default defineConfig({
   test: {
     coverage: {
       provider: 'istanbul',
+      include: ['**'],
+      exclude: [],
       reporter: ['text', 'json', 'html'],
+      all: true,
+      lines: 100,
+      functions: 100,
+      branches: 100,
+      statements: 100
     },
   },
 })
